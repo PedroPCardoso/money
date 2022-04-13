dbce7e7c2b0a0e0f8c6123341c09ee4086c4450c
pedropcardoso <pedroecomp@gmail.com>
2022-04-12 22:37:50 -0300

    Task-002 - Correção da chamada

--- money-backend/app/Http/Services/MoneyService.php
+++ money-backend/app/Http/Services/MoneyService.php
@@ -1,14 +1,13 @@
 <?php
 namespace App\Http\Services;
 use App\Models\Money;
-use App\Http\Saloon\Requests\GetForgeServerRequest;
 use App\Http\Integrations\Economia\Requests\CotacoesRequest;
 
 class MoneyService {
 
     public function conversorDeMoeda(string $moeda, float $valor) {
         if(Money::COTACOES[$moeda]){
-            $cotacao = $this->getCotacao(Money::COTACOES[$moeda]);
+            $cotacao = $this->getCotacaoDasMoedas(Money::COTACOES[$moeda]);
             
             $valorTotal = $valor * $cotacao;
             return [
@@ -21,7 +20,6 @@ class MoneyService {
     }
     public function getCotacaoDasMoedas($parDeMoedas){
  
-        // $saloon = new SaloonConnector();
         $request = new CotacoesRequest(parDeMoedas: $parDeMoedas);
         $cotacao= $request->send()->json()->low;
 
